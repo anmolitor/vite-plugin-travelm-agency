5634a6f790c0667fec10e4321dae55168a0f62b5
amolitor <andreas.molitor@andrena.de>
2025-04-10 00:23:05 +0200

    Implement multi app support

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ type EmitFile = (file: { filename: string; content: string }) => void;
 
 interface Options {
   translationDir: string;
-  defaultLanguage: string;
+  defaultLanguage: string | undefined;
   elmPath: string;
   generatorMode: "inline" | "dynamic";
   i18nArgFirst: boolean;
@@ -23,7 +23,7 @@ interface Options {
   prefixFileIdentifier: boolean;
 }
 
-export function travelmAgencyPlugin(options: Partial<Options>): Plugin {
+function addDefaults(options: Partial<Options>): Options {
   const translationDir = options.translationDir || "translations";
   const elmPath = options.elmPath || "src/Translations.elm";
   const generatorMode = options.generatorMode || "inline";
@@ -33,33 +33,59 @@ export function travelmAgencyPlugin(options: Partial<Options>): Plugin {
   const jsonPath = options.jsonPath || "i18n";
   const prefixFileIdentifier = !!options.prefixFileIdentifier;
 
-  const travelmOptions: T.Options =
-    generatorMode === "inline"
-      ? {
-          translationDir,
-          elmPath,
-          generatorMode,
-          i18nArgFirst,
-          addContentHash,
-          devMode: true,
-          prefixFileIdentifier,
-        }
-      : {
-          translationDir,
-          elmPath,
-          generatorMode,
-          i18nArgFirst,
-          addContentHash,
-          jsonPath,
-          devMode: true,
-          prefixFileIdentifier,
-        };
+  return {
+    translationDir,
+    elmPath,
+    generatorMode,
+    i18nArgFirst,
+    addContentHash,
+    jsonPath,
+    prefixFileIdentifier,
+    defaultLanguage: options.defaultLanguage,
+  };
+}
+
+function toTravelmAgencyOptions({
+  generatorMode,
+  translationDir,
+  elmPath,
+  i18nArgFirst,
+  addContentHash,
+  prefixFileIdentifier,
+  jsonPath,
+}: Options): T.Options {
+  return generatorMode === "inline"
+    ? {
+        translationDir,
+        elmPath,
+        generatorMode,
+        i18nArgFirst,
+        addContentHash,
+        devMode: true,
+        prefixFileIdentifier,
+      }
+    : {
+        translationDir,
+        elmPath,
+        generatorMode,
+        i18nArgFirst,
+        addContentHash,
+        jsonPath,
+        devMode: true,
+        prefixFileIdentifier,
+      };
+}
+
+export function travelmAgencyPlugin(
+  ...options: [Partial<Options>, ...Partial<Options>[]]
+): Plugin {
+  const withDefaults = options.map(addDefaults);
 
   const virtualModuleId = "virtual:travelm-agency";
   const resolvedVirtualModuleId = "\0" + virtualModuleId;
 
   const jsonFiles = new Map<string, string>();
-  const fileNameToReqPaths = new Map<string, string>();
+  const fileNameToReqPaths = new Map<string, [string, Options]>();
   const htmls = new Map<string, string>();
 
   let triggerReload = () => {};
@@ -71,47 +97,56 @@ export function travelmAgencyPlugin(options: Partial<Options>): Plugin {
   }
 
   async function runTravelmAgency(
+    opts: Options,
     translationFilePaths: string[],
     emitFile?: EmitFile
   ) {
     const devMode = emitFile === undefined;
-    const makeSureDirExists = fs.mkdir(path.dirname(elmPath), {
+    const makeSureDirExists = fs.mkdir(path.dirname(opts.elmPath), {
       recursive: true,
     });
     const travelmAgency = T.createInstance();
     await travelmAgency.sendTranslations(translationFilePaths, devMode);
     const responseContent = await travelmAgency.finishModule({
-      ...travelmOptions,
+      ...toTravelmAgencyOptions(opts),
       devMode,
     });
     await makeSureDirExists;
 
     const shouldBeWritten = await fs
-      .readFile(elmPath, {
+      .readFile(opts.elmPath, {
         encoding: "utf-8",
       })
       .then((data) => data !== responseContent.elmFile)
       .catch(() => true);
 
     if (shouldBeWritten) {
-      await fs.writeFile(elmPath, responseContent.elmFile);
+      await fs.writeFile(opts.elmPath, responseContent.elmFile);
     }
 
-    if (generatorMode === "dynamic") {
+    if (opts.generatorMode === "dynamic") {
       responseContent.optimizedJson.forEach((file) => {
         const expectedRequestPath =
           "/" +
-          path.normalize(`${jsonPath}/${file.filename}`).replace(path.sep, "/");
-        if (addContentHash) {
+          path
+            .normalize(`${opts.jsonPath}/${file.filename}`)
+            .replace(path.sep, "/");
+        if (opts.addContentHash) {
           const [identifier, language, _hash, _ext] = file.filename.split(".");
           const fileNameWithoutHash = [identifier, language].join(".");
           const oldReqPath = fileNameToReqPaths.get(fileNameWithoutHash);
-          oldReqPath && jsonFiles.delete(oldReqPath);
-          fileNameToReqPaths.set(fileNameWithoutHash, expectedRequestPath);
+          oldReqPath && jsonFiles.delete(oldReqPath[0]);
+          fileNameToReqPaths.set(fileNameWithoutHash, [
+            expectedRequestPath,
+            opts,
+          ]);
         } else {
           const [identifier, language, _ext] = file.filename.split(".");
           const fileNameWithoutHash = [identifier, language].join(".");
-          fileNameToReqPaths.set(fileNameWithoutHash, expectedRequestPath);
+          fileNameToReqPaths.set(fileNameWithoutHash, [
+            expectedRequestPath,
+            opts,
+          ]);
         }
         jsonFiles.set(expectedRequestPath, file.content);
         if (emitFile !== undefined) {
@@ -140,34 +175,43 @@ export function travelmAgencyPlugin(options: Partial<Options>): Plugin {
   return {
     name: "travelm-agency-plugin",
     buildStart: async function (this: PluginContext) {
-      const filePaths = await getTranslationFilePaths(translationDir);
-      function emitFile(
-        this: PluginContext,
-        file: { filename: string; content: string }
-      ) {
-        if (generatorMode === "inline") {
-          throw new Error(
-            "We somehow tried to emit a file in inline mode. This should not happen."
-          );
+      for (const opts of withDefaults) {
+        const filePaths = await getTranslationFilePaths(opts.translationDir);
+        function emitFile(
+          this: PluginContext,
+          file: { filename: string; content: string }
+        ) {
+          if (opts.generatorMode === "inline") {
+            throw new Error(
+              "We somehow tried to emit a file in inline mode. This should not happen."
+            );
+          }
+          this.emitFile({
+            type: "asset",
+            fileName: path.join(opts.jsonPath, file.filename),
+            source: file.content,
+          });
         }
-        this.emitFile({
-          type: "asset",
-          fileName: path.join(jsonPath, file.filename),
-          source: file.content,
-        });
+        const emit = this.meta.watchMode ? undefined : emitFile.bind(this);
+        await runTravelmAgency(opts, filePaths, emit);
       }
-      const emit = this.meta.watchMode ? undefined : emitFile.bind(this);
-      await runTravelmAgency(filePaths, emit);
     },
     handleHotUpdate: async function ({ file }) {
-      const relativePath = path.relative(path.resolve(translationDir), file);
-      if (relativePath.startsWith("..")) {
-        return;
-      }
-      const filePaths = await getTranslationFilePaths(translationDir);
-      await runTravelmAgency(filePaths);
-      if (isTranslationFileActive(relativePath)) {
-        triggerReload();
+      for (const opts of withDefaults) {
+        const relativePath = path.relative(
+          path.resolve(opts.translationDir),
+          file
+        );
+        if (relativePath.startsWith("..")) {
+          return;
+        }
+        const filePaths = await getTranslationFilePaths(opts.translationDir);
+        await runTravelmAgency(opts, filePaths);
+        if (isTranslationFileActive(relativePath)) {
+          triggerReload();
+        }
+        // Multiple apps should not overlap
+        break;
       }
     },
     resolveId(id) {
@@ -207,6 +251,7 @@ export function travelmAgencyPlugin(options: Partial<Options>): Plugin {
           );
           let newHtml = "";
           let lastIndex = 0;
+          let options = undefined;
 
           for (const match of matches) {
             newHtml += html.slice(lastIndex, match.index);
@@ -217,7 +262,13 @@ export function travelmAgencyPlugin(options: Partial<Options>): Plugin {
                 `Unknown bundleName/language '${bundleName}'/'${language}'. Correct syntax is __TRAVELM_AGENCY_bundleName__.`
               );
             }
-            const json = jsonFiles.get(reqPath);
+            if (options && reqPath[1].elmPath !== options.elmPath) {
+              throw new Error(
+                `Attempted to include bundles from two seperate applications: ${options.elmPath}, ${reqPath[1].elmPath}`
+              );
+            }
+            options = reqPath[1];
+            const json = jsonFiles.get(reqPath[0]);
             if (!json) {
               throw new Error(
                 `Could not find generated json for path ${reqPath}`
@@ -225,6 +276,12 @@ export function travelmAgencyPlugin(options: Partial<Options>): Plugin {
             }
             newHtml += json;
             lastIndex = match.index + match[0].length;
+          }
+
+          if (!options) {
+            throw new Error(
+              "Should be impossible because we checked for matches.length before"
+            );
           }
 
           newHtml += html.slice(lastIndex);
